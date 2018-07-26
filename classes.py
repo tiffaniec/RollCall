57d9222768b365bc5ed8364c354e1748d29e2751
Melody Ma <melody2ma@gmail.com>
2018-07-26 09:50:09 -0700

    added index py

--- classes.py
+++ classes.py
@@ -2,8 +2,8 @@ class cls_blk:
     string crse_name
     string teach_name
     int period
-    def _init_(self, crse_name,teach_name)
+    def _init_(self, crse_name,teach_name):
         self.crse_name = cse_name
         self.teach_name = teach_name
     def main():
-        block1=class_block
+        #block1=class_block
---
+++ index.py
@@ -0,0 +1,8 @@
+per_total = 1
+def sched(inputOne, inputTwo):
+    x = clsblk(crse_name= inputOne, teach_name = inputTwo, period=per_total)
+    append.schedulelist(x)
+While True:
+    onclick("add class"):
+        per_total +=1
+        sched()
