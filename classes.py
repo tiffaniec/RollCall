15e679c2af6eda9ed46044dff33ec9f41b264b44
jasmine <jasmine.m.2020@lighthousecharter.org>
2018-07-26 11:26:38 -0700

    contact

--- classes.py
+++ classes.py
@@ -23,7 +23,7 @@ class student
             usr_2 = student("melody",2020)
 <<<<<<< HEAD
 
-            
+
 =======
 >>>>>>> c7d3f100be5a01080d3c96f12d1afb7068897690
 >>>>>>> 252e546c115681803c707e85f2ef9df758501168
