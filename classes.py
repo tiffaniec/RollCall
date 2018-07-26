f45fd038aa174e51eceab6463d5dc7bf22ce711e
Melody Ma <melody2ma@gmail.com>
2018-07-26 09:42:53 -0700

    classes

---
+++ classes.py
@@ -0,0 +1,9 @@
+class cls_blk:
+    string crse_name
+    string teach_name
+    int period
+    def _init_(self, crse_name,teach_name)
+        self.crse_name = cse_name
+        self.teach_name = teach_name
+    def main():
+        block1=class_block
--- index.py
+++
@@ -1,7 +0,0 @@
-class cls_blk:
-    string crse_name
-    string teacher_name
-    int period
-    def _init_(self, crse_name,teach_name)
-    self.crse_name = course_name
-    self.teach_name
