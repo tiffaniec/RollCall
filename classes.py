c7d3f100be5a01080d3c96f12d1afb7068897690
jasmine <jasmine.m.2020@lighthousecharter.org>
2018-07-26 09:50:05 -0700

    class student

--- classes.py
+++ classes.py
@@ -5,5 +5,15 @@ class cls_blk:
     def _init_(self, crse_name,teach_name)
         self.crse_name = cse_name
         self.teach_name = teach_name
-    def main():
+    Def main():
         block1=class_block
+
+
+
+class student
+        def__init_(self,name,grad_year)
+        self.name=name
+        self.grad-yr= grad_yr
+        def main():
+            usr_1 = student("shreya",2019)
+            usr_2 = student("melody",2020)
