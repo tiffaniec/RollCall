0fd54bb351e21bc4d9b0961dde79014909c240f2
Melody Ma <melody2ma@gmail.com>
2018-07-26 11:42:43 -0700

    some stuff

--- classes.py
+++ classes.py
@@ -2,23 +2,21 @@ class cls_blk:
     string crse_name
     string teach_name
     int period
-    def _init_(self, crse_name,teach_name):
-        self.crse_name = cse_name
-        self.teach_name = teach_name
-<<<<<<< HEAD
-    def main():
-        #block1=class_block
-=======
-    Def main():
-        block1=class_block
+def _init_(self, crse_name,teach_name):
+    self.crse_name = cse_name
+    self.teach_name = teach_name
+
+def main():
+    #block1=class_block instance
 
 
 
-class student
-        def__init_(self,name,grad_year)
+class student:
+    string name
+    int grad_yr
+    def__init_(self, name, grad_year):
         self.name=name
         self.grad-yr= grad_yr
-        def main():
-            usr_1 = student("shreya",2019)
-            usr_2 = student("melody",2020)
->>>>>>> c7d3f100be5a01080d3c96f12d1afb7068897690
+    def main():
+        # usr_1 = student("shreya",2019) instance
+        # usr_2 = student("melody",2020) instance
